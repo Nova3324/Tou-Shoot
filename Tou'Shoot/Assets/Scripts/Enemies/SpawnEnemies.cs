8f7e97a98e72067b307969bfe2fdcc5938c0f302
Roux Cédric <c.roux@lahorde.school>
2023-11-22 19:45:06 +0100

    Spawn enemies
    - enemis spawn every t secondes

--- Tou'Shoot/Assets/Scripts/Enemies/SpawnEnemies.cs
+++ Tou'Shoot/Assets/Scripts/Enemies/SpawnEnemies.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Enemies : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private List<GameObject> m_enemyList = new List<GameObject>();
+    [SerializeField] private List<GameObject> m_spawnPoints = new List<GameObject>();
+    
+    [SerializeField] private int m_WaitForSpawn;
+    private int m_maxEnemies;
+    private int m_index = 0;
+    
+    private bool m_canSpawn = true;
     void Start()
     {
-        
+        m_maxEnemies = m_enemyList.Count;
+        StartCoroutine(SpawnEnemy());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator SpawnEnemy()
     {
-        
+        while (m_index < m_maxEnemies)
+        {
+            if (m_canSpawn)
+            {
+                Instantiate(m_enemyList[m_index], m_spawnPoints[m_index].transform.position, m_spawnPoints[m_index].transform.rotation);
+                m_index++;
+
+                m_canSpawn = false;
+                yield return new WaitForSeconds(m_WaitForSpawn);
+                m_canSpawn = true;
+            }
+            yield return null;
+        }
     }
 }
